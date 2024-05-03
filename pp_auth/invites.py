1aa6d387a6f5c78be9ccbc9f35690cdde3025cb5
Matt S <hmb.matt@gmail.com>
2024-05-02 22:59:45 -0700

    Integrated invite links to refactored app

--- pp_auth/invites.py
+++ pp_auth/invites.py
@@ -5,38 +5,30 @@ from flask import (
     redirect,
     render_template,
     session,
-    make_response,
-    request
+    make_response
 )
-from flask_login import (
-    login_user,
-    logout_user,
-    current_user
-)
+from flask_login import login_user, logout_user, current_user
 from config import *
-from models import User
+from pp_auth.models import User
 
 INVITE_LIMIT = 4
 invite_codes = []
 for i in range(INVITE_LIMIT):
     invite_codes.append(str(uuid.uuid4()))
 
-user = Blueprint("user", __name__)
+bp = Blueprint("invites", __name__)
 
 
-@user.route("/invite-codes", methods=["GET"])
+@bp.route("/invite-codes", methods=["GET"])
 def generate_invite():
     return render_template("generate.html", invite_codes = invite_codes)
 
 
-@user.route("/login/invite/<invite_code>", methods=["GET"])
+@bp.route("/login/invite/<invite_code>", methods=["GET"])
 def login_single_use_invite(invite_code):
     if current_user.is_authenticated:
         print("in endpoint /<invite_code>: current user is already authenticated")
         return redirect("/")
-
-    # print('in endpoint /<invite_code>: code is ' + invite_code)
-
     if invite_code in invite_codes:
         invite_codes.remove(invite_code)
         user = User(id=invite_code)
@@ -51,7 +43,7 @@ def login_single_use_invite(invite_code):
         return redirect("/")
 
 
-@user.route("/logout")
+@bp.route("/logout")
 def logout():
     if current_user.is_authenticated:
         logout_user()
